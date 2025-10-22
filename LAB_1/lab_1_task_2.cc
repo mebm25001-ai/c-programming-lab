d4580e835512381c2a3557eace16a0e533f51cbe
mebm25001-ai <mebm25001@tezu.ac.in>
2025-10-22 20:47:19 +0530

    Add files via upload

---
+++ LAB_1/lab_1_task_2.cc
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("my name is Prashant Sinha \n");
+    printf("my roll number is MEBM25001 \n");
+    printf("my branch is Mechanical Engineering \n");
+    return 0;
+}
